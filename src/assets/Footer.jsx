4912ffcb5fb01d120de90c3bc7f767ee4d7b9d60
Prakhar301101 <prakhar.pandey.pp6@gmail.com>
2023-04-24 14:58:40 +0530

    Add Readme file,Change Footer

--- src/assets/Footer.jsx
+++ src/assets/Footer.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
+
 export default function Footer() {
+  const date = new Date();
+  const year = date.getFullYear();
+
   return (
     <div className="bottom-container">
-      <a target="_blank" href='https://github.com/Prakhar301101'>© Prakhar Pandey 2023</a>
+      <a target="_blank" href='https://github.com/Prakhar301101'>© Prakhar Pandey {year}</a>
     </div>
   )
 }
