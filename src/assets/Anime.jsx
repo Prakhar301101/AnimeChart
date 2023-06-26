6dd83e4789cbcafb27c1744c5a3a364f55c3da87
Prakhar301101 <prakhar.pandey.pp6@gmail.com>
2023-06-26 19:27:36 +0530

    Add TBA in airing times

--- src/assets/Anime.jsx
+++ src/assets/Anime.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Image(props) {
   return (
     <div className="animeimg">
-      <h3>{props.Air}</h3>
+    {props.Air===("Unknown")||props.Air===null?<h3>TBA</h3>:<h3>{props.Air}</h3>}
       <a target="_blank" href={props.linkMal}>
         <img src={props.linkImg}></img>
       </a>
